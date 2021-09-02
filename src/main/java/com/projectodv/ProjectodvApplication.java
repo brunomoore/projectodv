a6c259506842ef2e96bd305f5fdb7d5fe92ada89
Bruno Moore <brunomoore22@gmail.com>
2021-09-01 21:05:59 -0300

    initial

---
+++ src/main/java/com/projectodv/ProjectodvApplication.java
@@ -0,0 +1,13 @@
+package com.projectodv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectodvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectodvApplication.class, args);
+	}
+
+}
